73ef99f41ac1d0934d892f5092948d5e8420cdf0
luis9157 <700567@fom-net.de>
2025-11-18 14:54:28 +0100

    Added view

--- my-vue-Tryoutapp/src/router/index.js
+++ my-vue-Tryoutapp/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
+import CreateJobView from '@/views/CreateJobView.vue'
+
 
 
 const router = createRouter({
@@ -23,6 +25,12 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView
+    },
+
+    {
+      path: '/create-job',
+      name: 'Job creation',
+      component: CreateJobView
     }
 
   ],
